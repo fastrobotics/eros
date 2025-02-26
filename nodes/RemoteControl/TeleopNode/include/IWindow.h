aa2a92674c86798ebcb287355e595cef97f55454
David Gitz <davidgitz@gmail.com>
2025-02-25 19:48:02 -0600

    WIP

--- nodes/RemoteControl/TeleopNode/include/IWindow.h
+++ nodes/RemoteControl/TeleopNode/include/IWindow.h
@@ -14,5 +14,6 @@ class IWindow
 
     virtual bool update(double dt, double t_ros_time) = 0;
     virtual KeyEventContainer new_keyevent(int key) = 0;
+    virtual std::vector<int> get_supported_keys() = 0;
 };
 }  // namespace eros_nodes::RemoteControl
