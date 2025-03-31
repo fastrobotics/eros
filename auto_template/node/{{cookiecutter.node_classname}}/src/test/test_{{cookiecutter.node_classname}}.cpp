f1fc0dc20d8e82f9c21b03a7001e22c068002adb
David Gitz <davidgitz@gmail.com>
2025-03-31 01:47:07 -0500

    Testing CI.

--- auto_template/node/{{cookiecutter.node_classname}}/src/test/test_{{cookiecutter.node_classname}}.cpp
+++ auto_template/node/{{cookiecutter.node_classname}}/src/test/test_{{cookiecutter.node_classname}}.cpp
@@ -17,6 +17,7 @@ void heartbeat_Callback(const eros::heartbeat& msg) {
     heartbeat_count++;
 }
 TEST({{cookiecutter.node_classname}}, TestBasics) {
+    ASSERT_TRUE(false);
     ros::NodeHandle nh("~");
     Logger* logger = new Logger("DEBUG", "test_{{cookiecutter.node_classname}}");
     logger->enable_ROS_logger();
